b3c87ed7e614c8ef4f22aaa92c403006489f4dc1
nglow90 <93581322+nglow90@users.noreply.github.com>
2021-12-05 13:14:10 +0900

    Temp

--- src/main/java/com/example/thymeleafbasic/BasicController.java
+++ src/main/java/com/example/thymeleafbasic/BasicController.java
@@ -1,9 +1,19 @@
 package com.example.thymeleafbasic;
 
+import lombok.Data;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -19,5 +29,56 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring</b>!");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        var userA = new User("userA", 10);
+        var userB = new User("userB", 20);
+
+        List<User> users = new ArrayList<>();
+        users.add(userA);
+        users.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", users);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello" + data;
+        }
+    }
+
+    @GetMapping("date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "Temp";
+    }
+
+    @Data
+    static class User {
+        public String username;
+        public Integer age;
+
+        public User(String username, Integer age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
